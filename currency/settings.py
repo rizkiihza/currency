6db8d5c856747a6a579316a5a0cf2e6873d41f3a
rizkiihza <rizki.ihza@gmail.com>
2019-02-14 16:48:02 +0700

    add mysql database' ;

--- currency/settings.py
+++ currency/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'currency.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'currency',
+        'USER': 'currency',
+        'PASSWORD': 'currency',
+        'HOST': 'localhost',
     }
 }
 
