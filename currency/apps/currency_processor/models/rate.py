13144317025179474e7ef4443a020423a666823c
rizkiihza <rizki.ihza@gmail.com>
2019-02-14 11:00:20 +0700

    testing the model

--- currency/apps/currency_processor/models/rate.py
+++ currency/apps/currency_processor/models/rate.py
@@ -4,11 +4,14 @@ from django.utils.timezone import now
 # Create your models here.
 class Rate(models.Model):
     rate_id = models.AutoField(primary_key=True)
-    currency_form = models.CharField(max_length=128)
+    currency_from = models.CharField(max_length=128)
     currency_to = models.CharField(max_length=128)
 
     value = models.DecimalField(max_digits=10, decimal_places=4)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField(default=now().date())
+
+    def __str__(self):
+        return "%s | %s to %s: %f" % (self.date, self.currency_from, self.currency_to, self.value)
 
 
 
